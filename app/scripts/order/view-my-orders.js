1028fb7de5085833ae54906c98eb982ed952c9df
Xindong Ding <dxd.spirits@gmail.com>
2015-12-16 03:52:28 +0800

    style navbar

--- app/scripts/order/view-my-orders.js
+++ app/scripts/order/view-my-orders.js
@@ -2,8 +2,24 @@
 
     var orders = new Amour.Collection();
 
+    var OrdersListView = Amour.CollectionView.extend({
+        ModelView: Amour.ModelView.extend({
+            events: { 'click': 'viewDetail' },
+            className: 'order-item',
+            template: App.getTemplate('order-item'),
+            viewDetail: function() {
+                App.router.navigate('order/' + this.model.id);
+            }
+        })
+    });
+
     App.Pages.MyOrders = new (App.PageView.extend({
-        initPage: function() {},
+        initPage: function() {
+            this.ordersListView = new OrdersListView({
+                collection: orders,
+                el: this.$('.orders-list')
+            })
+        },
         render: function() {
             var url = Amour.APIRootSecure + 'beacon/pay/getOrderListByWx.do';
             App.securePost(url, {
