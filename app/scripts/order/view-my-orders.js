ce1e5f714e9aa9f8c7afeda13a31fb56b623149d
Xindong Ding <dxd.spirits@gmail.com>
2015-12-22 02:28:12 +0800

    style order list items

--- app/scripts/order/view-my-orders.js
+++ app/scripts/order/view-my-orders.js
@@ -7,6 +7,11 @@
             events: { 'click': 'viewDetail' },
             className: 'order-item',
             template: App.getTemplate('order-item'),
+            serializeData: function() {
+                var data = this.model.toJSON();
+                data.statusName = ['等待付款', '已付款/代收货', '取消订单', '退款中', '退款成功'][data.status];
+                return data;
+            },
             viewDetail: function() {
                 App.router.navigate('order/' + this.model.id);
             }
@@ -27,6 +32,13 @@
             }, function(data) {
                 orders.reset(data, { parse: true });
             });
+            orders.reset([{
+                id: 3385, title: '复古牛仔背长裤', size: 'M', count: 3, amount: '999', status: 0,
+                image: 'http://123.57.253.146/images/a88bd3f1765faca77aa4edf910619bb3.jpg',
+            }, {
+                id: 3386, title: '复古牛仔背长裤', size: 'M', count: 3, amount: '999', status: 1,
+                image: 'http://123.57.253.146/images/a88bd3f1765faca77aa4edf910619bb3.jpg',
+            }]);
         }
     }))({el: $('#view-my-orders')});
 
