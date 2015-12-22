9e4fae58b6b65b616b1f9e985fa33415a4f4276e
Xindong Ding <dxd.spirits@gmail.com>
2015-12-22 23:44:48 +0800

    fix wechat auth

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -96,8 +96,7 @@
         var dataStr = App.encryptJSON(data);
         var model = new (Amour.Model.extend({
             parse: function(response) {
-                // response.replace(/\r\n/g, '');
-                return App.decryptJSON(response);
+                return response ? App.decryptJSON(response) : {};
             }
         }))();
         model.save({
